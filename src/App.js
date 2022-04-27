6ea67431cadcd79350cb22f441e9eca7165fd4f9
Oliver Fox <ofoxavondale@gmail.com>
2022-04-27 10:50:03 +0100

    add construction placeholder

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Aside from './components/Aside';
 
+import Placeholder from './components/Placeholder'
+
 function App() {
   return (
   <div id="grid-container">
-      <Header />
+    <Placeholder />
+      {/* <Header />
       <Hero />
       <About />
       <Experience />
       <Portfolio />
       <Contact />
       <Footer />
-      <Aside />
+      <Aside /> */}
   </div>
   );
 }
---
+++ src/components/Placeholder.js
@@ -0,0 +1,10 @@
+
+const Placeholder =() => {
+    return (
+        <div id="placeholder">
+            <img src="../fox-icon.svg" /><h1>Under Construction</h1>
+        </div>
+    )
+}
+
+export default Placeholder;
