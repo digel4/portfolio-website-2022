18467218a5021b1bd2de06cdbeffb84c0b01f0ab
digel4 <ofoxavondale@gmail.com>
2022-05-13 08:19:46 +0100

    add other portfolio projects

--- src/components/ResumeDocument.js
+++ src/components/ResumeDocument.js
@@ -1,9 +1,7 @@
 import { Document, Page, pdfjs  } from 'react-pdf';
 import React, { useState } from 'react';
 import resume from '../resume.pdf';
-import myResume from "../resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-//import { Document, Page } from  'react-pdf/dist/entry.parcel';
 
 
 
@@ -26,20 +24,11 @@ const Resume = (props) => {
     return (
         <div id="resume" className="resume-hide">
             <div id="resume-container">
-               
-                <Document  file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-                    <Page className="document" pageNumber={pageNumber} />
+                
+                <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+                    <Page width="20px" height="20px" pageNumber={pageNumber} />
                 </Document>
                 <label onClick={ () => {hideResume(); } }htmlFor="Document">&#88;</label>
-                <div className="buttons">
-                    <a className="button" href={myResume} target="_blank" rel="noreferrer">See File</a>
-                    <div className="button" onClick={ () => {hideResume(); } }>Back</div>
-                </div>
-
-
-                {/* <PDFDownloadLink document={<ResumeDocument />} fileName="Olive-Fox-CV.pdf">
-                    {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-                </PDFDownloadLink> */}
             </div>
         </div>
     )
